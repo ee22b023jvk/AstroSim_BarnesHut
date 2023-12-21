9a86db5bec42bbb372467404b7b79585632d6310
ee22b023jvk <133376250+ee22b023jvk@users.noreply.github.com>
2023-12-21 13:06:57 +0530

    Add files via upload

--- AstroSim-2.0-1/ForceCalculators/BarnesHut.cpp
+++ AstroSim-2.0-1/ForceCalculators/BarnesHut.cpp
@@ -1,15 +1,16 @@
 #include "BarnesHut.h"
 #include <cassert>
-
+#include <algorithm>
+#include <vector>
 class Node{
             public:
-                long double mass = 0; // total mass of node
-                vector<long double> com; // centre of mass of node
+                valtype mass = 0; // total mass of node
+                vector<valtype> com; // centre of mass of node
                 vector<GravitationalBody*> particles; //ptrs to particles inside the node
-                vector<long double> centre; // centre of node
-                long double s; // side length
+                vector<valtype> centre; // centre of node
+                valtype s; // side length
                 vector<Node*> child; // ptrs to child nodes
-                Node(long double x, long double y, long double z, long double side){
+                Node(valtype x, valtype y, valtype z, valtype side){
                     centre = {x,y,z};
                     s = side;
                 }
@@ -18,10 +19,10 @@ class Node{
 class BarnesHutMethods {
     private:
         void Divide(Node* node){
-            long double x = node->centre[0];
-            long double y = node->centre[1];
-            long double z = node->centre[2];
-            long double s = node->s;
+            valtype x = node->centre[0];
+            valtype y = node->centre[1];
+            valtype z = node->centre[2];
+            valtype s = node->s;
             Node* btr = new Node(x+s/4,y+s/4,z-s/4,s/2); // b-bottom t-top l-left r-right
             Node* btl = new Node(x-s/4,y+s/4,z-s/4,s/2);
             Node* bbl = new Node(x-s/4,y-s/4,z-s/4,s/2);
@@ -34,9 +35,9 @@ class BarnesHutMethods {
         }
 
         int whichOctant(Node* node, GravitationalBody& body){
-            long double xr = body.position[0] - node->centre[0];
-            long double yr = body.position[1] - node->centre[1];
-            long double zr = body.position[2] - node->centre[2];
+            valtype xr = body.position[0] - node->centre[0];
+            valtype yr = body.position[1] - node->centre[1];
+            valtype zr = body.position[2] - node->centre[2];
             if (xr>0 && yr>0 && zr<0){return 0;}
             else if (xr<0 && yr>0 && zr<0){return 1;}
             else if (xr<0 && yr<0 && zr<0){return 2;}
@@ -108,19 +109,19 @@ class BarnesHutMethods {
             delete root;
         }
 
-        vector<long double> TreeForce(Node* node, GravitationalBody& body){                 //G needs to be 6.67*10-34
-            long double fx=0, fy=0, fz=0;
+        vector<valtype> TreeForce(Node* node, GravitationalBody& body){                 //G needs to be 6.67*10-34
+            valtype fx=0, fy=0, fz=0;
             if (node->particles.size()==1){
                 if (node->particles[0]!=&body){
-                    long double r = Q_rsqrt(((node->com[0]-body.position[0]),2)+pow((node->com[1]-body.position[1]),2)+pow((node->com[2]-body.position[2]),2));
+                    valtype r = Q_rsqrt(((node->com[0]-body.position[0]),2)+pow((node->com[1]-body.position[1]),2)+pow((node->com[2]-body.position[2]),2));
                     fx = G*body.mass*node->mass*(node->com[0]-body.position[0])/pow(r,3);
                     fy = G*body.mass*node->mass*(node->com[1]-body.position[1])/pow(r,3);
                     fz = G*body.mass*node->mass*(node->com[2]-body.position[2])/pow(r,3);                   
                 }
             }
             else {
-                long double r = Q_rsqrt(pow((node->com[0]-body.position[0]),2)+pow((node->com[1]-body.position[1]),2)+pow((node->com[2]-body.position[2]),2));
-                long double theta = 0.5;
+                valtype r = Q_rsqrt(pow((node->com[0]-body.position[0]),2)+pow((node->com[1]-body.position[1]),2)+pow((node->com[2]-body.position[2]),2));
+                valtype theta = 0.5;
                 if (node->s/r <theta){
                     fx = G*body.mass*node->mass*(node->com[0]-body.position[0])/pow(r,3);
                     fy = G*body.mass*node->mass*(node->com[1]-body.position[1])/pow(r,3);
@@ -128,7 +129,7 @@ class BarnesHutMethods {
                 }
                 else {
                     for (auto it=node->child.begin();it!=node->child.end();it++){
-                        vector<long double> ChildForce = TreeForce(*it,body);
+                        vector<valtype> ChildForce = TreeForce(*it,body);
                         fx+=ChildForce[0];
                         fy+=ChildForce[1];
                         fz+=ChildForce[2];
@@ -139,31 +140,42 @@ class BarnesHutMethods {
         }
 
     public:
-        vector<long double> RootCenter;
-        long double RootSide;
-        BarnesHutMethods(vector<long double> center, long double side){
-            RootCenter = center;
-            RootSide = side;
+        vector<valtype> RootCenter;
+        valtype RootSide;
+        valtype ComputeRootSide(vector<GravitationalBody>& o){
+            valtype side = 0;
+            for (int i=0;i<o.size();i++){
+                //side = max({side,(valtype)2*abs(o[i].position[0]),(valtype)2*abs(o[i].position[1]),(valtype)2*abs(o[i].position[2])});
+                for(int j = 0; j < 3; j++){
+                    if(side < 2*abs(o[i].position[j])){
+                        side = 2*abs(o[i].position[j]);
+                    }
+                }
+            }
+            return side;
         }
-        vector<vector<long double>> BarnesHutForces(vector<GravitationalBody>& o){
+        BarnesHutMethods(vector<valtype> center, vector<GravitationalBody>& o){
+            RootCenter = center;
+            RootSide = ComputeRootSide(o);
+        }
+        vector<vector<valtype>> BarnesHutForces(vector<GravitationalBody>& o){
             Node* root = new Node(RootCenter[0],RootCenter[1],RootCenter[2],RootSide);
             OcttreeBuild(root,o);
-            vector<long double> fx(o.size(),0), fy(o.size(),0), fz(o.size(),0);
+            vector<valtype> fx(o.size(),0), fy(o.size(),0), fz(o.size(),0);
             for (int i=0;i<o.size();i++){
-                vector<long double> ForceOnBody = TreeForce(root,o[i]);
+                vector<valtype> ForceOnBody = TreeForce(root,o[i]);
                 fx[i] = ForceOnBody[0];
                 fy[i] = ForceOnBody[1];
                 fz[i] = ForceOnBody[2];
             }
 
-            //cout << fx[0] << endl;                      //wrong
             DeleteOcttree(root);
             return {fx,fy,fz};
         }
 };
 
 BarnesHut::BarnesHut(GravitationalSystem& s):ForceCalculator(s){
-    BarnesHutMethods TreeObj({0, 0, 0}, 100);                         //need to change based on length scale of the problem(rename later)
+    BarnesHutMethods TreeObj({0, 0, 0}, s.bodies);                         //need to change based on length scale of the problem(rename later)
     forces = TreeObj.BarnesHutForces(s.bodies);
 }
 
